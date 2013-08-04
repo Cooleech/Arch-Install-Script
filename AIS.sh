d06ee6cc67793d554c9c7b5b3391d9786553274b
Cooleech <cooleech@gmail.com>
2013-08-04 07:41:02 +0200

    Update AIS.sh

--- AIS.sh
+++ AIS.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #################################
 # What:  Arch-Install-Script   #
-# Which: version 6.3          #
+# Which: version 6.4          #
 # Who:   Cooleech            #
 # Under: GPLv2                #
 # Contact: cooleechATgmail.com #
@@ -17,38 +17,37 @@ clear
 if [ "$Korisnik" = "" ]; then
  Korisnik="archie"
  echo "
- Niste upisali ništa pa je korisničko ime postavljeno automatski:
-
+ Niste upisali ništa pa je korisničko ime postavljeno automatski:"
+fi
+echo "
  Korisničko ime: $Korisnik
 
  Prihvaćate li to ime? (D/n)
- "
- read Prihvat
- Prihvat="${Prihvat,,}" # Konverzija u lowercase
- clear
- case "$Prihvat" in
- n*)
- clear
- USER_NAME
- esac
-fi
+"
+read Prihvat
+Prihvat="${Prihvat,,}" # Konverzija u lowercase
+clear
+case "$Prihvat" in
+n*)
+clear
+USER_NAME
+esac
 }
-
 function ENTER_USER_PASS {
 clear
 Lozinka1=""
 Lozinka2=""
-echo "Upišite lozinku za korisnika $Korisnik (neće prikazati unos):"
+echo " Upišite lozinku za korisnika $Korisnik (neće prikazati unos):"
 stty -echo
 read Lozinka1
 stty echo
 if [ "$Lozinka1" = "" ]; then
- read -p "Lozinka ne može biti prazna!
+ read -p " Lozinka ne može biti prazna!
  
  Pritisnite Enter za nastavak..."
  ENTER_USER_PASS
 fi
-echo "Ponovo upišite lozinku za korisnika $Korisnik (neće prikazati unos):"
+echo " Ponovo upišite lozinku za korisnika $Korisnik (neće prikazati unos):"
 stty -echo
 read Lozinka2
 stty echo
@@ -56,28 +55,27 @@ if [ "$Lozinka1" = "$Lozinka2" ]; then
  LozinkaKorisnika="$Lozinka1
 $Lozinka2"
 else
- read -p "Lozinke se ne podudaraju!
+ read -p " Lozinke se ne podudaraju!
 	
  Pritisnite Enter za nastavak..."
  ENTER_USER_PASS
 fi
 }
-
 function ENTER_ROOT_PASS {
 clear
 Lozinka3=""
 Lozinka4=""
-echo "Upišite lozinku za root korisnika (neće prikazati unos):"
+echo " Upišite lozinku za root korisnika (neće prikazati unos):"
 stty -echo
 read Lozinka3
 stty echo
 if [ "$Lozinka3" = "" ]; then
- read -p "Lozinka ne može biti prazna!
+ read -p " Lozinka ne može biti prazna!
  
  Pritisnite Enter za nastavak..."
  ENTER_ROOT_PASS
 fi
-echo "Ponovo upišite lozinku za root korisnika (neće prikazati unos):"
+echo " Ponovo upišite lozinku za root korisnika (neće prikazati unos):"
 stty -echo
 read Lozinka4
 stty echo
@@ -85,13 +83,12 @@ if [ "$Lozinka3" = "$Lozinka4" ]; then
  RootLozinka="$Lozinka3
 $Lozinka4"
 else
- read -p "Lozinke se ne podudaraju!
+ read -p " Lozinke se ne podudaraju!
 	
  Pritisnite Enter za nastavak..."
  ENTER_ROOT_PASS
 fi
 }
-
 function PARTITIONING {
 echo "
  Prikaz dostupnih diskova:"
@@ -114,7 +111,6 @@ if [ $? != 0 ]; then
  PARTITIONING
 fi
 }
-
 function SEL_ROOT_PARTITION {
 clear
 lsblk /dev/$OdabraniDisk
@@ -124,16 +120,17 @@ echo "
 read RootPart
 if [ "$RootPart" = "" ]; then
  clear
- echo " *** GREŠKA ***
+ read -p " *** GREŠKA ***
 
- Root particija *MORA* biti odabrana!"
+ Root particija *MORA* biti odabrana!
+ 
+ Enter za nastavak..."
  SEL_ROOT_PARTITION
 fi
 }
-
 function NET_DEVICE {
 rm -f /etc/udev/rules.d/80-net-name-slot.rules # Ako već postoji
-ln -sf /dev/null /etc/udev/rules.d/80-net-name-slot.rules # Preimenuj mrežne uređaje u "stara" imena
+ln -s /dev/null /etc/udev/rules.d/80-net-name-slot.rules # Preimenuj mrežne uređaje u "stara" imena
 clear
 echo "
  Pregled dostupnih mrežnih uređaja:
@@ -154,23 +151,26 @@ fi
 eth*)
 ip link set $NetUredjaj up
 if [ $? != 0 ]; then
- echo " Netočno ime mrežnog uređaja!"
- sleep 2
+read -p " Netočno upisano ime uređaja!
+
+ Enter za nastavak..."
  NET_DEVICE
 fi
 ;;
 "")
 clear
-echo " Niste upisali mrežni uređaj!
- To je nužno za uspješan nastavak instalacije!"
-sleep 2
+read -p " Niste upisali mrežni uređaj!
+ To je nužno za uspješan nastavak instalacije!
+
+ Enter za nastavak..."
 NET_DEVICE
 ;;
 *)
 ip link set $NetUredjaj up
 if [ $? != 0 ]; then
- echo " Netočno upisano ime uređaja!"
- sleep 2
+ read -p " Netočno upisano ime uređaja!
+
+ Enter za nastavak..."
  NET_DEVICE
 fi
 echo " Bežična mreža? (d/N)"
@@ -186,10 +186,9 @@ Bezicno="${Bezicno,,}" # To lowercase
 ;;
 esac
 }
-
 #==============================================================================#
 loadkeys croat # Postavljam tipkovnicu na hrvatski layout
-ln -sf /dev/null /etc/udev/rules.d/80-net-name-slot.rules # Provjerit ima li smisla da bude i ovde
+ln -s /dev/null /etc/udev/rules.d/80-net-name-slot.rules # Preimenuj mrežne uređaje u "stara" imena
 setfont Lat2-Terminus16 # Postavljam font (podržava sva naša slova)
 clear
 echo "
@@ -222,24 +221,18 @@ echo " Upišite brojku particije za swap
 read SwapPart
 SwapPart="${SwapPart//'/dev/$OdabraniDisk'/}" # Za svaki slučaj... :)
 if [ "$HomePart" != "" ]; then
- Home="Home: /dev/$OdabraniDisk$HomePart
+ Home=" Home: /dev/$OdabraniDisk$HomePart
  "
  OpcijaHome=" (opcionalno za /home)"
 fi
 if [ "$SwapPart" != "" ]; then
- Swap="Swap: /dev/$OdabraniDisk$SwapPart
+ Swap=" Swap: /dev/$OdabraniDisk$SwapPart
  "
 fi
 clear
-lsblk /dev/$OdabraniDisk
-echo "
- Pregled postavljenih particija:
-
- Root: /dev/$OdabraniDisk$RootPart
- $Home$Swap
-"
 echo " Upišite ime hosta (bez razmaka, prazno za archlinux):
- Pritisnite Enter za nastavak ili Ctrl + C za prekid...
+
+ Enter za nastavak ili Ctrl + C za prekid...
 "
 read ImeHosta
 ImeHosta="${ImeHosta// /}" # Ukloni razmake
@@ -258,7 +251,7 @@ if [ $? != 0 ]; then
  Nema dostupne internet veze! :( Provjerite kabel ili postavke mrežnog uređaja!
  Da bi se uspješno izvršila, ova instalacija TREBA vezu s internetom!
  "
- read -p " Pritisnite Enter za nastavak ili Ctrl + C za prekid..."
+ read -p " Enter za nastavak ili Ctrl + C za prekid..."
  NET_DEVICE
 fi
 clear
@@ -272,7 +265,6 @@ SudoOvlasti="sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/g' /etc/sudoe
 "")
 SudoOvlasti="sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/g' /etc/sudoers"
 esac
-
 echo " Želite li koristiti wicd za upravljanje mrežnim uređajima? (d/N)"
 read HocuWicd
 HocuWicd="${HocuWicd,,}"
@@ -288,6 +280,17 @@ echo " Odaberite jedan od ponuđenih DE-a za instalaciju
  5 za Awesome
 "
 read DEzaInst
+case $DEzaInst in
+2*)
+echo " Želite li pri podizanju sustava automatski biti ulogirani kao $Korisnik? (d/N)"
+read AutoLogin
+AutoLogin="${AutoLogin,,}"
+;;
+3*)
+echo " Želite li pri podizanju sustava automatski biti ulogirani kao $Korisnik? (d/N)"
+read AutoLogin
+AutoLogin="${AutoLogin,,}"
+esac
 echo " Ovo računalo je prijenosno? (d/N)"
 read Prijenosnik
 Prijenosnik="${Prijenosnik,,}"
@@ -296,9 +299,23 @@ d*)
 TouchpadDriver=" xf86-input-synaptics libsynaptics"
 esac
 clear
-echo " Ok, to bi bilo sve. Imam dovoljno informacija za nastavak instalacije.
- Samo sjednite i opustite se dok se instalacija ne obavi do kraja. ;)
+echo "____________________________
+ Pregled osnovnih postavki:
+----------------------------
+Korisničko ime: $Korisnik
+Ime hosta: $ImeHosta
+Particije diska:
 "
+lsblk /dev/$OdabraniDisk
+
+read -p " Root: /dev/$OdabraniDisk$RootPart
+$Home$Swap
+ To bi bilo to. Imam dovoljno informacija za nastavak instalacije.
+ Samo sjednite i opustite se dok se instalacija ne obavi do kraja. ;)
+
+Enter za nastavak ili Ctrl + C za prekid...
+"
+clear
 echo " Formatiranje particija..."
 if [ "$HomePart" != "" ]; then
  case "$Formatiraj" in
@@ -336,25 +353,15 @@ pacstrap /mnt base base-devel
 echo " Generiranje fstab datoteke..."
 genfstab -p /mnt | sed 's/rw,relatime,data=ordered/defaults,relatime/' >> /mnt/etc/fstab
 echo " Stvaram ArchChroot skriptu..."
-
 #==============================================================================#
 echo "#!/bin/sh
 #################################
 # What:  ArchChroot            #
-# Which: version 6.3          #
+# Which: version 6.4          #
 # Who:   Cooleech            #
 # Under: GPLv2                #
 # Contact: cooleechATgmail.com #
 #################################
-
-case \"$DEzaInst\" in
-2*)
-echo \"
-[mate]
-SigLevel = Optional TrustAll
-Server = http://repo.mate-desktop.org/archlinux/$(uname -m)\" >> /etc/pacman.conf
-esac
-
 ln -s /dev/null /etc/udev/rules.d/80-net-name-slot.rules
 loadkeys croat
 echo \"$RootLozinka\" > /tmp/rootpass
@@ -398,7 +405,7 @@ case \"\$HocuWicd\" in
 d*)
 echo \"Instaliram wicd i wicd-gtk...\"
 pacman -S --noconfirm wicd wicd-gtk
-systemctl -f enable wicd.service
+systemctl enable wicd.service
 gpasswd -a $Korisnik network
 ;;
 *)
@@ -407,7 +414,6 @@ pacman -S --noconfirm ifplugd
 echo \" Konfiguriram netctl...\"
 cp /etc/netctl/examples/ethernet-dhcp /etc/netctl/
 netctl enable ethernet-dhcp
-netctl start ethernet-dhcp ########################### Mislim da na ovo javlja da je u chrootu i da će ignorirat
 systemctl enable netctl-ifplugd@ethernet-dhcp.service
 ;;
 esac
@@ -420,33 +426,47 @@ $SudoOvlasti
 case \"$DEzaInst\" in
 1*)
 echo \" Pokrećem minimalnu instalaciju KDE-a...\"
-pacman -S --noconfirm kdebase kdebase-workspace kdemultimedia-kmix oxygen-gtk2 oxygen-gtk3 phonon-gstreamer # opcionalno: kde-l10n-en_us kde-l10n-hr_hr
-systemctl -f enable kdm.service
+pacman -S --noconfirm kdebase kdebase-workspace kdemultimedia-kmix oxygen-gtk2 oxygen-gtk3 phonon-gstreamer # kde-l10n-en_us kde-l10n-hr_hr
+systemctl enable kdm.service
 echo \" Stvaram .xinitrc datoteku u mapi /home/$Korisnik/\"
 echo \"exec startkde\" >> /home/$Korisnik/.xinitrc
 ;;
 2*)
 echo \" Pokrećem instalaciju MATE-a...\"
-pacman -S --noconfirm mate mate-extras gtk-engine-murrine slim
-systemctl -f enable slim.service
+echo \"
+[mate]
+SigLevel = Optional TrustAll
+Server = http://repo.mate-desktop.org/archlinux/$(uname -m)\" >> /etc/pacman.conf
+pacman -Sy --noconfirm mate mate-extras gtk-engine-murrine slim
+systemctl enable slim.service
 echo \" Dodajem korisnika $Korisnik na listu SLiM login managera...\"
 sed -i 's/#default_user        simone/default_user        $Korisnik/g' /etc/slim.conf
+case \"\$AutoLogin\" in
+d*)
+echo \" Postavljam auto-login...\"
+sed -i 's/#auto_login          no/auto_login          yes/g' /etc/slim.conf
+esac
 echo \" Stvaram .xinitrc datoteku u mapi /home/$Korisnik/\"
 echo \"exec mate-session\" >> /home/$Korisnik/.xinitrc
 ;;
 3*)
 echo \" Pokrećem instalaciju Xfce4 DE-a...\"
-pacman -S --noconfirm deadbeef file-roller parole thunar-archive-plugin thunar-volman xfce4 xfce4-goodies xfce4-notifyd zenity slim #gvfs-smb
-systemctl -f enable slim.service
+pacman -S --noconfirm deadbeef file-roller parole thunar-archive-plugin thunar-volman xfce4 xfce4-goodies xfce4-notifyd zenity slim # gvfs-smb
+systemctl enable slim.service
 echo \" Dodajem korisnika $Korisnik na listu SLiM login managera...\"
 sed -i 's/#default_user        simone/default_user        $Korisnik/g' /etc/slim.conf
+case \"\$AutoLogin\" in
+d*)
+echo \" Postavljam auto-login...\"
+sed -i 's/#auto_login          no/auto_login          yes/g' /etc/slim.conf
+esac
 echo \" Stvaram .xinitrc datoteku u mapi /home/$Korisnik/\"
 echo \"exec startxfce4\" >> /home/$Korisnik/.xinitrc
 ;;
 4*)
 echo \" Pokrećem instalaciju LXDE-a...\"
 pacman -S --noconfirm file-roller lxde lxdm leafpad gnome-mplayer gnome-themes-standard zenity
-systemctl -f enable lxdm.service
+systemctl enable lxdm.service
 echo \" Stvaram .xinitrc datoteku u mapi /home/$Korisnik/\"
 echo \"exec startlxde\" >> /home/$Korisnik/.xinitrc
 ;;
@@ -458,21 +478,19 @@ echo \"exec awesome\" >> /home/$Korisnik/.xinitrc
 \"\")
 echo \" INFO:
 
- Nije instaliran nikakav DE/WM!\"
+ Neće biti instaliran nikakav DE/WM!\"
 ;;
 *)
 echo \" INFO:
 
- Nije instaliran nikakav DE/WM!\"
+ Neće biti instaliran nikakav DE/WM!\"
 ;;
 esac
 rm -f /root/.bashrc
 rm -f /etc/ArchChroot
 echo \" Upišite exit za izlaz...\"
-exit" > /mnt/etc/ArchChroot
-
+exit 0" > /mnt/etc/ArchChroot
 #==================================================================================================#
-clear
 echo "sh /etc/ArchChroot" > /mnt/root/.bashrc
 arch-chroot /mnt /bin/bash
 clear
@@ -491,18 +509,4 @@ echo "
  Sretno uz novoinstaliran Arch! :)
 "
 read -p " Enter za reboot..."
-reboot && eject
-
-### DODATAK SLiM-u - IZBAČENO ALI RADI ###
-#echo \"
-# Želite li pri podizanju sustava automatski biti ulogirani kao $Korisnik? (d/N)\"
-#read AutoLogin
-#AutoLogin=\"\${AutoLogin,,}\"
-#case \"\$AutoLogin\" in
-#d*)
-#echo \" Postavljam auto-login...\"
-#sed -i 's/#auto_login          no/auto_login          yes/g' /etc/slim.conf
-#;;
-#*)
-#;;
-#esac 
+reboot
